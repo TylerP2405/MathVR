eca421d44fb72075595b81d2f334d26e45ff0f2c
Mysha-rah <rajitarah79@gmail.com>
2025-03-14 18:09:39 -0500

    Add files via upload

---
+++ Assets/Whiteboard/Scripts/Whiteboard Scripts.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Collection;
+using System.Collection.Generic;
+
+
+public class WhiteboardScripts : MonoBehaviour
+{
+    public Texture2D texture;
+    public Vector2 textureSize = new Vector2(2048, 2048);
+    void Start()
+    {
+        var r = GetComponent<Renderer>();
+        texture = new Texture2D((int)textureSize.x, (int)textureSize.y);
+        r.material.mainTexture = texture;
+    }
+
+
+}
